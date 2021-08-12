ad2374d58ca0c6325f19beb5f76d6ee92d0e5590
hsmon <hso92.bb@gmail.com>
2021-08-12 21:37:31 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod helloworld;
 mod json;
+mod path_json;
+mod path_params;
 
 fn main() {
     // helloworld::main();
-    json::main();
+    // json::main();
+    // path_params::main();
+    path_json::main();
 }
---
+++ src/path_json.rs
@@ -0,0 +1,34 @@
+use axum::{extract, prelude::*, response};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+
+#[tokio::main]
+pub async fn main() {
+  let app = route("/users/:user_id", post(user_message));
+  let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+  hyper::Server::bind(&addr)
+    .serve(app.into_make_service())
+    .await
+    .unwrap();
+}
+
+#[derive(Deserialize)]
+struct Name {
+  name: String,
+}
+
+#[derive(Serialize)]
+struct Message {
+  message: String,
+}
+
+async fn user_message(
+  extract::UrlParams(params): extract::UrlParams<(u64,)>,
+  extract::Json(name): extract::Json<Name>,
+) -> response::Json<Message> {
+  let user_id = params.0;
+  let name = name.name;
+  response::Json(Message {
+    message: format!("Hello {}, your id is {}", name, user_id),
+  })
+}
