2cdb4361922688c930bcaa6f097560fe8017d2b4
hsmon <hso92.bb@gmail.com>
2021-08-15 15:15:23 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+mod from_request;
 mod helloworld;
 mod json;
 mod path_json;
 mod path_params;
+mod response;
+mod state;
 
 fn main() {
     // helloworld::main();
     // json::main();
     // path_params::main();
-    path_json::main();
+    // path_json::main();
+    // from_request::main();
+    // state::main();
+    response::main();
 }
